594b363504a638b0364c9d8a52477605225c2b95
Jon Garrett <jonathan.garrett@gmail.com>
2016-09-01 13:57:27 -0700

    Add pin option to map.jsx markers

--- client/Components/Map.jsx
+++ client/Components/Map.jsx
@@ -16,15 +16,24 @@ class Map extends React.Component {
     console.log('mounted', ReactDOM.findDOMNode(this));
 
     var pos = {lat: 37.7837678, lng: -122.40914660000001};
+
     var map = new google.maps.Map(ReactDOM.findDOMNode(this), {
       center: pos,
       zoom: 8
     });
 
-    var infoWindow = new google.maps.InfoWindow({map: map});
-    infoWindow.setContent('Location found.');
-    infoWindow.setPosition(pos);
+    var infoWindow = new google.maps.InfoWindow({
+      map: map,
+      position: pos,
+      content: 'You Are Here'
+    });
     console.log(infoWindow);
+    // We can have info windows and pins
+    // TODO: Use some fun emoji for our pins!
+    var marker = new google.maps.Marker({
+      map: map,
+      position: pos
+    });
   }
 
   render() {
