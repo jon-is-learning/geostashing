905832e29d15252d5485ee567d3b9922ae41b0ed
turbio <masongclayton@gmail.com>
2016-09-01 21:47:36 -0700

    fix test styling

--- test/server.js
+++ test/server.js
@@ -7,23 +7,23 @@ chai.use(chaiHttp);
 describe('server', () => {
   describe('static files', () => {
     it('should GET /', (done) => {
-      chai.request(server).get('/').then(res => {
-          res.should.have.status(200);
-          done();
+      chai.request(server).get('/').then((res) => {
+        res.should.have.status(200);
+        done();
       }).catch(done);
     });
 
     it('should GET / with valid html', (done) => {
-      chai.request(server).get('/').then(res => {
+      chai.request(server).get('/').then((res) => {
         res.should.have.status(200);
-        res.should.be.html
+        res.should.be.html;
         done();
       }).catch(done);
     });
 
     it('should return 404 for /notafile', (done) => {
       chai.request(server).get('/notafile').then(done)
-      .catch(res => {
+      .catch((res) => {
         res.should.have.status(404);
         done();
       });
@@ -33,7 +33,7 @@ describe('server', () => {
   describe('geolocations api endpoint (/api/geolocations)', () => {
     it('should return json from GET /api/geolocations', (done) => {
       chai.request(server)
-        .get('/api/geolocations').then(res => {
+        .get('/api/geolocations').then((res) => {
           res.should.have.status(200);
           res.should.be.json;
           done();
@@ -42,7 +42,7 @@ describe('server', () => {
 
     it('should return an array from GET /api/geolocations', (done) => {
       chai.request(server)
-        .get('/api/geolocations').then(res => {
+        .get('/api/geolocations').then((res) => {
           res.should.have.status(200);
           res.body.should.be.an('array');
           done();
@@ -53,7 +53,7 @@ describe('server', () => {
       chai.request(server)
         .post('/api/geolocations')
         .send({ name: 'test', lat: 0, lng: 0 })
-        .then(res => {
+        .then((res) => {
           res.should.have.status(200);
           res.body.should.be.empty;
           done();
@@ -63,16 +63,19 @@ describe('server', () => {
     it('should reject post requests to /api/geolocations with invalid data', (done) => {
       chai.request(server).post('/api/geolocations')
         .then(done)
-        .catch(res => {
+        .catch((res) => {
           res.should.have.status(400);
+
           return chai.request(server).post('/api/geolocations').send({ thisis: 'not valid', 'test': ['data']});
-        }).catch(res => {
+        }).catch((res) => {
           res.should.have.status(400);
+
           return chai.request(server).post('/api/geolocations').send({});
-        }).catch(res => {
+        }).catch((res) => {
           res.should.have.status(400);
+
           return chai.request(server).post('/api/geolocations').send('also not valid');
-        }).catch(res => {
+        }).catch((res) => {
           res.should.have.status(400);
           done();
         });
@@ -81,17 +84,18 @@ describe('server', () => {
     xit('should persist and return posted geolocations', (done) => {
       var reqData = { name: 'first test', lat: 123, lng: 321 };
       chai.request(server).post('/api/geolocations')
-        .send(reqData)
-        .then(res => {
-          res.should.have.status(200);
-          return chai.request(server).get('/api/geolocations');
-        }).then(res => {
-          res.should.be.json;
-          res.body.should.be.an('array');
-          res.body.length.should.not.equal(0);
-          res.body.should.contain(reqData);
-          done();
-        }).catch(done);
+      .send(reqData)
+      .then((res) => {
+        res.should.have.status(200);
+
+        return chai.request(server).get('/api/geolocations');
+      }).then((res) => {
+        res.should.be.json;
+        res.body.should.be.an('array');
+        res.body.length.should.not.equal(0);
+        res.body.should.contain(reqData);
+        done();
+      }).catch(done);
     });
   });
 });
