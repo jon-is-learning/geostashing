46a6bcaf8c787b910a95f47107e8e15a3d0145a5
turbio <masongclayton@gmail.com>
2016-09-05 23:29:53 -0700

    use 500 for server errors

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -4,13 +4,13 @@ const userController = {
   getOne (req, res) {
     User.findOne(req.body)
       .then((user) => res.status(200).send(user))
-      .catch((err) => res.status(400).send(err));
+      .catch((err) => res.status(500).send(err));
   },
 
   addOne (req, res) {
     User.create(req.body)
       .then((user) => res.status(200).send(user))
-      .catch((err) => res.status(400).send(err));
+      .catch((err) => res.status(500).send(err));
   }
 };
 
