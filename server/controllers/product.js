c1433a4f85bc3b5f9efb246881ee2349268abd9f
turbio <masongclayton@gmail.com>
2016-09-08 04:38:56 -0700

    get /api/products should return products with location and user

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,10 +1,13 @@
 const Product = require('../models/productModel');
+const User = require('../models/userModel');
+const Location = require('../models/locationModel');
 
 const productController = {
   getAll(req, res) {
-    Product.findAll()
+    Product.findAll({ include: [User, Location] })
       .then((products) => res.json(products));
   },
+
   addOne(req, res) {
     Product.create(req.body)
       .then((product) => res.json(product))
