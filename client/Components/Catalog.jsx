a5c93c9ca6f713fb616e48ca0a7b9d26d3fc3663
turbio <masongclayton@gmail.com>
2016-09-08 05:46:27 -0700

    catalog filtering

--- client/Components/Catalog.jsx
+++ client/Components/Catalog.jsx
@@ -2,15 +2,54 @@ import React from 'react';
 import Product from './Product.jsx';
 
 class Catalog extends React.Component {
-  render() {
-    console.log(this.props.products);
+  constructor(props) {
+    super(props);
+
+    this.state = { filter: null };
+  }
+
+  updateFilter(str) {
+    let filter = null;
+    let regexErr = false;
+
+    if (str.length > 0) {
+      try {
+        filter = new RegExp(str, 'gi');
+      } catch (err) {
+        regexErr = true;
+      }
+    }
+
+    this.setState({ filter, regexErr });
+  }
+
+  filterProduct(product) {
+    if (!this.state.filter) {
+      return true;
+    }
 
     return (
+        product.name
+        + product.description
+        + product.seller.name
+        + product.location.name
+      ).match(this.state.filter);
+  }
+
+  render() {
+    return (
       <div className="catalog">
-        <input type="text" name="search" placeholder="search"/>
+        <input
+          onChange={() => this.updateFilter(this.refs.search.value)}
+          ref="search"
+          type="text"
+          name="search"
+          placeholder="search"/>
         <ul className="products">
           {
-            this.props.products.map((product, index) =>
+            this.props.products
+              .filter(this.filterProduct.bind(this))
+              .map((product, index) =>
                 <Product key={index} info={product}/>)
           }
         </ul>
