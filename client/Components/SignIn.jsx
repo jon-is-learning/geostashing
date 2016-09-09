204ff681cbcfb1740dba6e295ce263a283cd4859
Daniel Snyder <daniel.c.snyder@gmail.com>
2016-09-09 13:59:19 -0700

    Set up basic LogIn structure

--- client/Components/SignIn.jsx
+++ client/Components/SignIn.jsx
@@ -1,15 +1,69 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, withRouter } from 'react-router';
 
-const SignIn = () =>
-    <div>
-      <h1>SIGN IN</h1>
-      <h4>Username</h4>
-      <input type="text" />
-      <h4>Password</h4>
-      <input type="password" />
-      <input type="submit" />
-      <Link to="signup">Register</Link>
-    </div>;
+import auth from './../auth.js';
+
+const SignIn = withRouter (
+  React.createClass({
+    getInitialState() {
+      return {
+        error: null,
+        username: '',
+        password: ''
+      }
+    },
+
+    userNameInfoChange (e) {
+      this.setState({
+        username: e.target.value
+      })
+    },
+
+    userPasswordChange (e) {
+      this.setState({
+        password: e.target.value
+      })
+    },
+
+    checkLogin(e) {
+      e.preventDefault();
+
+      console.log(this.state.username);
+      console.log(this.state.password);
+    },
+
+
+    render () {
+      return (
+        <div>
+          <form onSubmit={this.checkLogin}>
+            <h1>SIGN IN</h1>
+            <h4>Username</h4>
+            <input type="text" onChange={this.userNameInfoChange}/>
+            <h4>Password</h4>
+            <input type="password" onChange={this.userPasswordChange} />
+            <input type="submit" />
+          </form>
+          <Link to="signup">Register</Link>
+        </div>
+      )
+
+    }
+  })
+
+);
 
 export default SignIn;
+
+
+
+// () =>
+//     <div>
+//       <h1>SIGN IN</h1>
+//       <h4>Username</h4>
+//       <input type="text" />
+//       <h4>Password</h4>
+//       <input type="password" />
+//       <input type="submit" />
+//       <Link to="signup">Register</Link>
+//     </div>;
