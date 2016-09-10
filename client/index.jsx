1ee41058611c3aaa452828eae8c055d89e91131f
turbio <masongclayton@gmail.com>
2016-09-09 17:13:11 -0700

    indent routing

--- client/index.jsx
+++ client/index.jsx
@@ -8,8 +8,8 @@ import SignUp from './Components/SignUp.jsx';
 
 ReactDOM.render(
 <Router history={hashHistory}>
-<Route path="/"component={App}></Route>
-<Route path="signin" component={SignIn}></Route>
-<Route path="signup" component={SignUp}></Route>
+  <Route path="/"component={App}></Route>
+  <Route path="signin" component={SignIn}></Route>
+  <Route path="signup" component={SignUp}></Route>
 </Router>,
 document.getElementById('app'));
