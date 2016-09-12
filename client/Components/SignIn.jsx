e36415bcef9f0f07bf996139cece17a7f918150c
Mason Clayton <turbio@users.noreply.github.com>
2016-09-12 12:03:38 -0700

    Revert "Revert "Add the session and cookie functionality""

--- client/Components/SignIn.jsx
+++ client/Components/SignIn.jsx
@@ -1,53 +1,57 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
-import $ from 'jquery';
+import auth from './../auth.js';
 
-const SignIn = withRouter(
-  React.createClass({
-    getInitialState() {
-      return {
-        error: false,
-        username: '',
-        password: ''
-      };
-    },
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
 
-    userPasswordChange(event) {
-      this.setState({ password: event.target.value });
-    },
+    this.state = { error: false };
+  }
 
-    checkLoginInfo(event) {
-      event.preventDefault();
-      $.ajax({
-        method: 'GET',
-        url: '/api/users/',
-        dataType: 'json',
-        success: (data) => {
-          console.log('DATA: ', data);
-        },
-        error: (error) => {
-          console.log('ERROR: ', error);
-        }
-      });
-    },
+  checkLoginInfo(event) {
 
-    render() {
-      return (
-        <div>
-          <h1>SIGN IN</h1>
-          <form onSubmit={this.checkLoginInfo}>
-            <h4>Username</h4>
-            <input ref="username" type="text" />
-            <h4>Password</h4>
-            <input ref="password" type="password" />
-            <input type="submit" />
-          </form>
-          <Link to="signup" className="link">Register</Link>
-        </div>
-      );
-    }
-  })
+    event.preventDefault();
+    const username = this.refs.username.value;
+    const password = this.refs.password.value;
 
-);
+    auth.login(username, password, (loggedIn) => {
+      if (!loggedIn) {
+        console.log('Not loggedin');
 
-export default SignIn;
+        return this.setState({ error: true });
+      }
+
+      const { location } = this.props;
+
+      if (location.state && location.state.nextPathname) {
+        return this.props.router.replace(location.state.nextPathname);
+      }
+
+      return this.props.router.replace('/home');
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>SIGN IN</h1>
+        <form onSubmit={this.checkLoginInfo.bind(this)}>
+          <h4>Username</h4>
+          <input ref="username" type="text" />
+          <h4>Password</h4>
+          <input ref="password" type="password" />
+          <input type="submit" />
+        </form>
+        <Link to="signup">Register</Link>
+      </div>
+    );
+  }
+}
+
+SignIn.propTypes = {
+  location: React.PropTypes.object,
+  router: React.PropTypes.object
+};
+
+export default withRouter(SignIn);
